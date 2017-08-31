098dda85643597264718e49771231af4d9ded2e7
Juan <ing.jca@hotmail.com>
2017-08-30 20:39:23 -0500

    configuracion heroku

--- src/app/models/recurso.ts
+++ src/app/models/recurso.ts
@@ -3,6 +3,7 @@ export class Recurso{
     constructor(
         public _id: string,
         public nombre: string,
+        public descripcion: string,
         public fechaCreacion: Date,
         public estado: Boolean,
         public ruta: string,
